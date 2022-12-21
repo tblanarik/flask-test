dd68b48247fe0a4ae07e4597f711ef0786dfae50
Trevor Blanarik <tblanarik@github.com>
2022-12-21 00:48:49 +0000

    autopep8 newlines and EOF lines

--- src/flaskr/db.py
+++ src/flaskr/db.py
@@ -2,6 +2,7 @@ import sqlite3
 import click
 from flask import current_app, g
 
+
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
@@ -12,11 +13,13 @@ def get_db():
 
     return g.db
 
+
 def close_db(e=None):
     db = g.pop('db', None)
 
     if db is not None:
         db.close()
+
 
 def init_db():
     db = get_db()
@@ -30,6 +33,7 @@ def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
     click.echo('Initialized the database.')
+
 
 def init_app(app):
     app.teardown_appcontext(close_db)
